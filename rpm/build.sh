24356e49e0d4db50a0e87fe44e9a9e4e293555a0
andrii.zagriadskyi <azystem@gmail.com>
2025-04-16 13:06:01 +0300

    add rpm build

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cp ../target/release/torrust-actix pkg/opt/torrust-actix
-/usr/local/bin/fpm -s dir -t rpm \
+docker build -t fpm .
+docker run -v $(pwd):/src fpm -s dir -t rpm \
   -n torrust-actix \
-  -v 1.0.0 \
-  --description "Self-hosted BitTorrent tracker torrust-actix" \
+  -v 4.0.10 \
+  --description "Torrust-Actix Tracker is a lightweight but incredibly powerful and feature-rich BitTorrent Tracker made using Rust." \
   --license "MIT" \
   --url "https://github.com/torrust/torrust" \
   --after-install postinstall.sh \
